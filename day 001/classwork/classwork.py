493ad2ff89c0bd9317f1a77a345b5ecae8a370fc
tornikeey <atuashvilitornike@gmail.com>
2025-06-09 14:16:10 +0400

    second commit

---
+++ day 001/classwork/classwork.py
@@ -0,0 +1,16 @@
+#Print ფუნქცია ბეჭდავს ფრჩხილებში გადაცემულ ატრიბუტს, ეს ატრიბუტი გამოდის ტერმინალში.
+
+City="rustavi"
+year=2025
+country="georgia"
+age=17
+
+print(City)
+print(year)
+print(country)
+print(age)
+
+#String,("Data") 
+# Integer, (80)
+# Float (5.12)
+# Boolean (True,False)
